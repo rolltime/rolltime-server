ecb2bf286f39ebe5a4a6ff75e9a3176f1d38ec43
Luis Capelo <luiscape@gmail.com>
2015-09-14 19:54:06 -0400

    adding default values for the social media auth keys

--- config/auth.js
+++ config/auth.js
@@ -1,20 +1,20 @@
 // expose our config directly to our application using module.exports
 module.exports = {
   'facebookAuth': {
-    'clientID': process.env.FACEBOOK_CLIENT_ID,
-    'clientSecret': process.env.FACEBOOK_CLIENT_SECRET,
-    'callbackURL': process.env.CALLBACK_URL + '/auth/facebook/callback'
+    'clientID': process.env.FACEBOOK_CLIENT_ID || 'test',
+    'clientSecret': process.env.FACEBOOK_CLIENT_SECRET || 'test',
+    'callbackURL': process.env.CALLBACK_URL + '/auth/facebook/callback' || 'test'
   },
 
   'twitterAuth': {
-    'consumerKey': process.env.TWITTER_CONSUMER_KEY,
-    'consumerSecret': process.env.TWITTER_CONSUMER_SECRET,
-    'callbackURL': process.env.CALLBACK_URL + '/auth/twitter/callback'
+    'consumerKey': process.env.TWITTER_CONSUMER_KEY || 'test',
+    'consumerSecret': process.env.TWITTER_CONSUMER_SECRET || 'test',
+    'callbackURL': process.env.CALLBACK_URL + '/auth/twitter/callback' || 'test'
   },
 
   'googleAuth': {
-    'clientID': process.env.GOOGLE_CLIENT_ID,
-    'clientSecret': process.env.GOOGLE_CLIENT_SECRET,
-    'callbackURL': process.env.CALLBACK_URL + '/auth/google/callback'
+    'clientID': process.env.GOOGLE_CLIENT_ID || 'test',
+    'clientSecret': process.env.GOOGLE_CLIENT_SECRET || 'test',
+    'callbackURL': process.env.CALLBACK_URL + '/auth/google/callback' || 'test'
   }
 }
