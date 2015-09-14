c540425aa35c28fbc1bbd534dea156276b962d48
Luis Capelo <luiscape@gmail.com>
2015-09-13 21:35:29 -0400

    linting and adding monngo checker. closes #22

--- server.js
+++ server.js
@@ -1,44 +1,67 @@
-// server.js
-
-// set up ======================================================================
-// get all the tools we need
-
+//
+// Dependencies.
+//
+var morgan = require('morgan')
 var express = require('express')
-var app = express()
-var port = process.env.PORT || 8080
 var mongoose = require('mongoose')
 var passport = require('passport')
 var flash = require('connect-flash')
-
-var morgan = require('morgan')
-var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var session = require('express-session')
+var cookieParser = require('cookie-parser')
 
-var configDB = require('./config/database.js')
+//
+// Configuration variables.
+//
+var app = express()
+var port = process.env.PORT || 8080
+var DB = require('./config/database')
 
-// configuration ===============================================================
-mongoose.connect(configDB.url) // connect to our database
+//
+// Only start the application
+// if the database is ready.
+//
+mongoose.connection.on('connected', function (ref) {
+  //
+  // Log, parse request, and alike.
+  //
+  app.use(morgan('dev'))
+  app.use(cookieParser())
+  app.use(bodyParser.json())
+  app.set('view engine', 'ejs')
+  app.use(express.static('public'))
+  app.use(bodyParser.urlencoded({ extended: false }))
 
-require('./config/passport')(passport) // pass passport for configuration
+  //
+  // Start sessions with
+  // a password.
+  //
+  require('./config/passport')(passport) // pass passport for configuration
+  app.use(session({ secret: process.env.SESSION_KEY || 'rolltimekey' }))
+  app.use(passport.initialize())
+  app.use(passport.session())
 
-// set up our express application
-app.use(morgan('dev')) // log every request to the console
-app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()) // get information from html forms
-app.use(express.static('static')); // serving static files.
+  app.use(flash())
 
-app.set('view engine', 'ejs') // set up ejs for templating
+  //
+  // Routes (with passport).
+  //
+  require('./app/routes.js')(app, passport)
 
-// required for passport
-app.use(session({ secret: 'rolltimepassword' })) // session secret
-app.use(passport.initialize())
-app.use(passport.session()) // persistent login sessions
-app.use(flash()); // use connect-flash for flash messages stored in session
+  //
+  // Start server.
+  //
+  app.listen(port)
+  console.log('Scraper Status API running on port ' + port)
 
-// routes ======================================================================
-require('./app/routes.js')(app, passport) // load our routes and pass in our app and fully configured passport
+})
 
-// launch ======================================================================
-app.listen(port)
-console.log('Rolltime server running on port ' + port)
+mongoose.connection.on('error', function (err) {
+  console.log('Could not connect to MongoDB.')
+  throw err
+})
+
+//
+// Attempt connection with MongoDB.
+//
+mongoose.connect(DB.url)
