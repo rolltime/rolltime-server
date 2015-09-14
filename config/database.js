1e20e84184e32b077f2d59d89509fff3b08827c1
Luis Capelo <luiscape@gmail.com>
2015-09-14 19:54:31 -0400

    changing the default name of the password variable

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@
 module.exports = {
   'url': 'mongodb://' +
     process.env.MONGODB_USER_NAME + ':' +
-    process.env.MONGODB_PASSWORD + '@' +
+    process.env.MONGODB_USER_PASSWORD + '@' +
     process.env.MONGODB_LOCATION_URL + '/' +
     process.env.MONGODB_DATABASE
 }
