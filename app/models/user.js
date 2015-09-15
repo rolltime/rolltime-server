d127988fc3857373a95efbebd55a0b4ef0fad610
Luis Capelo <luiscape@gmail.com>
2015-09-14 21:56:22 -0400

    added new fields to the user schema

--- app/models/user.js
+++ app/models/user.js
@@ -1,47 +1,54 @@
-// app/models/user.js
-// load the things we need
-var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
+var mongoose = require('mongoose')
+var bcrypt = require('bcrypt-nodejs')
 
-// define the schema for our user model
-var userSchema = mongoose.Schema({
+//
+// User schema for Mongo.
+//
+var UserSchema = mongoose.Schema({
+  local: {
+    name: String,
+    zipcode: [],
+    phone: String,
+    email: String,
+    password: String
+  },
+  facebook: {
+    id: String,
+    token: String,
+    email: String,
+    name: String
+  },
+  twitter: {
+    id: String,
+    token: String,
+    displayName: String,
+    username: String
+  },
+  google: {
+    id: String,
+    token: String,
+    email: String,
+    name: String
+  }
 
-    local            : {
-        name         : String,
-        email        : String,
-        password     : String
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    }
+})
 
-});
+//
+// Hashing passwords at all times.
+//
+userSchema.methods.generateHash = function (password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null)
+}
 
-// methods ======================
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
+//
+// Checking if password is valid with bcrypt.
+//
+userSchema.methods.validPassword = function (password) {
+  return bcrypt.compareSync(password, this.local.password)
+}
 
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
-
-// create the model for users and expose it to our app
-module.exports = mongoose.model('User', userSchema);
+//
+// Register the model with Mongo
+// and export it to the application.
+//
+module.exports = mongoose.model('User', UserSchema)
